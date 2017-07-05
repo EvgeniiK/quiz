312c10ef5c6c465b33f5bddfa3f153a132b638cc
Yevhenii <lordlenoks@gmail.com>
2017-07-06 01:17:25 +0300

    add reports table parser

--- quiz.rb
+++ quiz.rb
@@ -2,7 +2,29 @@ require 'mechanize'
 require 'nokogiri'
 
 class Quiz
-  def run
 
+  EMAIL = 'test@example.com'.freeze
+  PASS  = 'secret'.freeze
+
+  def run
+    hash = {}
+    Mechanize.new.get('https://staqresults.herokuapp.com') do |home_page|
+      reports = home_page.form_with(id: 'form-signin') do |form|
+        form.email    = EMAIL
+        form.password = PASS
+      end.submit
+
+      reports.search('tbody > tr').each do |row|
+        coloumns = row.search('td')
+          hash[coloumns[0].text] = {
+            tests: coloumns[1].text,
+            passes: coloumns[2].text,
+            failures: coloumns[3].text,
+            pending: coloumns[4].text,
+            coverage: coloumns[5].text
+          }
+      end
+    end
+    hash
   end
 end
